fa0053fcc768b6b0bd23f260e4a07f5a287c8df0
Morten Daniel Fornes <mdfornes@gmail.com>
2020-06-01 13:20:25 +0700

    nsis: check %PROGRAMW6432%\nodejs #75

--- update-npm.cmd
+++ update-npm.cmd
@@ -1,14 +1,16 @@
 @echo off
 
-set NPM=%APPDATA%\npm\npm.cmd
+set DIR=%APPDATA%\npm
+set NPM=%DIR%\npm.cmd
 set PATH=%PROGRAMFILES%\nodejs;%PATH%
+set PATH=%PROGRAMW6432%\nodejs;%PATH%
 
 if exist "%NPM%" (
     exit /b 0
 )
 
 echo %NPM% not found
-npm install npm -g -f
+npm install npm -g -f --prefix "%DIR%"
 
 if exist "%NPM%" (
     exit /b 0
