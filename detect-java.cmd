6a2578c061002cd252145119fa20a2af72f53608
Morten Daniel Fornes <mdfornes@gmail.com>
2020-06-01 13:56:17 +0700

    nsis: update detect-java.cmd

--- detect-java.cmd
+++ detect-java.cmd
@@ -1,10 +1,7 @@
 @echo off
 
-if "%JAVA_HOME%" neq "" (
-    exit /b 0
-)
-
 set PATH=%PROGRAMW6432%\Android\Android Studio\jre\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%
 
 where java
 exit /b %ERRORLEVEL%
